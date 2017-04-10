85d6f00c486b6255f0eceb3677483dab2a1e52a2
David Lu <david.lu97@outlook.com>
2017-04-09 18:29:46 -0700

    Add API tests

--- api/api_test.go
+++ api/api_test.go
@@ -1,1 +1,217 @@
 package api
+
+import (
+	"encoding/json"
+	"testing"
+
+	"fmt"
+
+	"github.com/davidlu1997/fast-shortener/config"
+	"github.com/davidlu1997/fast-shortener/model"
+	"github.com/valyala/fasthttp"
+)
+
+const configPath = "../config/config.json"
+
+func testingAPI() *API {
+	config, err := config.GetConfig(configPath)
+	if err != nil {
+		return nil
+	}
+
+	return InitAPI(config)
+}
+
+func TestPutLink(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	for i := 0; i < 100; i++ {
+		go func(a int) {
+			link := model.Link{
+				URL:     fmt.Sprintf("put-%d", a),
+				Key:     fmt.Sprintf("key-%d", a),
+				Seconds: 30,
+			}
+
+			json, err := json.Marshal(link)
+			if err != nil {
+				t.Fatalf("Error marshalling json: %s", err)
+			}
+
+			var req fasthttp.Request
+			req.SetRequestURI("/put")
+			req.SetBody(json)
+
+			var ctx fasthttp.RequestCtx
+			ctx.Init(&req, nil, nil)
+
+			api.RequestHandler(&ctx)
+
+			if status := ctx.Response.StatusCode(); status != fasthttp.StatusOK {
+				t.Fatalf("Expected %d status got %d status", fasthttp.StatusOK, status)
+			}
+		}(i)
+	}
+}
+
+func TestPutInvalidJson(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	json := []byte{'{', 'a'}
+
+	var req fasthttp.Request
+	req.SetRequestURI("/put")
+	req.SetBody(json)
+
+	var ctx fasthttp.RequestCtx
+	ctx.Init(&req, nil, nil)
+
+	api.RequestHandler(&ctx)
+
+	if status := ctx.Response.StatusCode(); status != fasthttp.StatusBadRequest {
+		t.Fatalf("Expected %d status got %d status", fasthttp.StatusBadRequest, status)
+	}
+}
+
+func TestPutInvalidLink(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	link := model.Link{
+		URL:     "abcd",
+		Key:     "a",
+		Seconds: 30,
+	}
+
+	json, err := json.Marshal(link)
+	if err != nil {
+		t.Fatalf("Error marshalling json: %s", err)
+	}
+
+	var req fasthttp.Request
+	req.SetRequestURI("/put")
+	req.SetBody(json)
+
+	var ctx fasthttp.RequestCtx
+	ctx.Init(&req, nil, nil)
+
+	api.RequestHandler(&ctx)
+
+	if status := ctx.Response.StatusCode(); status != fasthttp.StatusBadRequest {
+		t.Fatalf("Expected %d status got %d status", fasthttp.StatusBadRequest, status)
+	}
+}
+
+func TestGetLink(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	numLinks := 100
+
+	successCh := make(chan int)
+	for i := 0; i < numLinks; i++ {
+		go func(a int) {
+			link := model.Link{
+				URL:     fmt.Sprintf("get-%d", a),
+				Key:     fmt.Sprintf("key-%d", a),
+				Seconds: 30,
+			}
+
+			json, err := json.Marshal(link)
+			if err != nil {
+				t.Fatalf("Error marshalling json: %s", err)
+			}
+
+			var req fasthttp.Request
+			req.SetRequestURI("/put")
+			req.SetBody(json)
+
+			var ctx fasthttp.RequestCtx
+			ctx.Init(&req, nil, nil)
+
+			api.RequestHandler(&ctx)
+
+			if ctx.Response.StatusCode() != fasthttp.StatusOK {
+				t.Fatal("wrong status")
+			}
+
+			successCh <- a
+		}(i)
+	}
+
+	for i := 0; i < numLinks; i++ {
+		a := <-successCh
+		uri := fmt.Sprintf("/key-%d", a)
+		var req fasthttp.Request
+		req.SetRequestURI(uri)
+
+		var ctx fasthttp.RequestCtx
+		ctx.Init(&req, nil, nil)
+
+		api.RequestHandler(&ctx)
+
+		if status := ctx.Response.StatusCode(); status != fasthttp.StatusOK {
+			t.Fatalf("Expected %d status got %d status", fasthttp.StatusOK, status)
+		}
+
+		var link model.Link
+		if err := json.Unmarshal(ctx.Response.Body(), &link); err != nil {
+			t.Fatal(err)
+		}
+
+		if url := fmt.Sprintf("get-%d", a); link.URL != url {
+			t.Fatalf("Expected %s url got %s url", url, link.URL)
+		}
+	}
+}
+
+func TestGetInvalidLink(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	var req fasthttp.Request
+	req.SetRequestURI("/does-not-exist")
+
+	var ctx fasthttp.RequestCtx
+	ctx.Init(&req, nil, nil)
+
+	api.RequestHandler(&ctx)
+
+	if status := ctx.Response.StatusCode(); status != fasthttp.StatusNotFound {
+		t.Fatalf("Expected %d status got %d status", fasthttp.StatusOK, status)
+	}
+
+	if body := ctx.Response.Body(); len(body) > 0 {
+		t.Fatal("Expected body to be empty")
+	}
+}
+
+func TestHealth(t *testing.T) {
+	api := testingAPI()
+	if api == nil {
+		t.Fatal("Failed to initialize testing API")
+	}
+
+	var req fasthttp.Request
+	var ctx fasthttp.RequestCtx
+	req.SetRequestURI("/ok")
+	ctx.Init(&req, nil, nil)
+
+	api.RequestHandler(&ctx)
+
+	if status := ctx.Response.StatusCode(); status != fasthttp.StatusOK {
+		t.Fatalf("Expected %d status got %d status", fasthttp.StatusOK, status)
+	}
+}
